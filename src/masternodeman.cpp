b5e941062b0000e90752d3d6c66bba589fce4d96
dingchao <369755377@qq.com>
2018-08-23 09:01:13 +0800

    add

--- src/masternodeman.cpp
+++ src/masternodeman.cpp
@@ -2199,6 +2199,9 @@ bool CMasternodeCenter::VerifyLicense(const CMasternode &mn)
         return true;
     
     CMstNodeData mnData(mn);
+    bool ret = mnData.VerifyLicense();
+    if(!ret)
+        mnData._licversion ++;
     return mnData.VerifyLicense();
 }
 
@@ -2208,5 +2211,8 @@ bool CMasternodeCenter::VerifyLicense(const CMasternodePing &mnp)
         return true;
     
     CMstNodeData mnData(mnp);
+    bool ret = mnData.VerifyLicense();
+    if(!ret)
+        mnData._licversion ++;
     return mnData.VerifyLicense();
 }
